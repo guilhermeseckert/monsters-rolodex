17ba6407b8993537fe7347922119ede690abbf7c
Guilherme Sonnemann Eckert <guilhermeeckert@Guilhermes-MacBook-Pro.local>
2020-07-07 21:40:56 -0700

    Updates

---
+++ precache-manifest.660664827b769e523e4e13588c071442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6018013fd929ee3abfbb2ac7e2463fe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4a9be46d3c4a1d90639",
+    "url": "/monsters-rolodex/static/css/main.a47cf25a.chunk.css"
+  },
+  {
+    "revision": "31b6ea61111ed388404a",
+    "url": "/monsters-rolodex/static/js/2.41a49e5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.41a49e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a9be46d3c4a1d90639",
+    "url": "/monsters-rolodex/static/js/main.8d9e2769.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
